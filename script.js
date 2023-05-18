e660eb6c46e48bb68f4e5e9388fc94930eaa36b1
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-18 11:04:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let person1 = {
+    name: 'John',
+    age: 30
+};
+
+let person2 = {
+    name: 'cj',
+    age: 23
+};
+
+console.log(person1);
+console.log(person2);
+
+let people = ['person1', 'person2'];
+
+console.log(people);
